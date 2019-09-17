73449113ed228634680a6e93429f64cec4339551
TyAus1999 <auslitztyler@gmail.com>
2019-09-17 10:23:26 -0400

    Added Store Controller and details page

---
+++ ControlAltPC/Controllers/StoreController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControlAltPC.Controllers
+{
+    public class StoreController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details(string product)//asp-route-product
+        {
+            //store the selected product name in the view bag container
+            //View bag is automatically passed back to the view that is loaded
+            ViewBag.product = product;
+
+            return View();
+        }
+    }
+}
